5ac06748b2108e80f88687851e235fc57b92b846
theoneandonlywoj <theoneandonlywoj@gmail.com>
2025-03-04 21:32:01 +0100

    feat: code quality deps

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule XLSXComposer.MixProject do
       version: "0.1.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      aliases: aliases()
     ]
   end
 
@@ -21,8 +22,14 @@ defmodule XLSXComposer.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false}
+    ]
+  end
+
+  defp aliases() do
+    [
+      quality: ["format --check-formatted", "credo --strict", "dialyzer"]
     ]
   end
 end
