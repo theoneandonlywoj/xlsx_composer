0b450af44019d1785b12079e92782cd5716593b5
theoneandonlywoj <theoneandonlywoj@gmail.com>
2025-03-15 14:18:39 +0100

    docs: typos

--- lib/xlsx_composer/section.ex
+++ lib/xlsx_composer/section.ex
@@ -2,7 +2,7 @@ defmodule XLSXComposer.Section do
   @moduledoc """
   #{__MODULE__} allows to group cells into units that we can reason and test on that level.
 
-  WARNING! : new limited number of Sections otherwise the process of the checks might hurt your performance!
+  WARNING! : limit number of Sections otherwise the process of the checks might hurt your performance!
   Soon, implementation of checking of Sections overlap will have to be done so use Section carefully.
   """
   alias XLSXComposer.CellDef
